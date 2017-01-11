5e3c337a49fed28bb2b032e020d54cfc949126fa
patyk <patyk@DESKTOP-RHDUJH3>
2017-01-11 15:45:17 +0100

    Add project files.

---
+++ FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using FirstApp.Models;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+			/* var hour = DateTime.Now.Hour;
+			 ViewBag.Greetings = hour < 17 ? "Dzien dobry" : "Dobranoc";
+			*/
+			return View();
+        }
+
+		
+	    public ActionResult RsvpForm()
+	    {
+		    return View();
+	    }
+
+		[HttpPost]
+	    public ActionResult RsvpForm(GuestResponse guestResponse)
+	    {
+		    return ModelState.IsValid ? View("Thanks",guestResponse) : View();
+	    }
+
+
+    }
+}
---
+++ FirstApp/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstApp.Models
+{
+
+	public class GuestResponse
+	{
+		[Required(ErrorMessage = "Podaj imie człowieku ;/")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Podaj email czlowieku"), EmailAddress(ErrorMessage = "To nie adres świnko ;/")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Podaj telefon kolego")]
+		public string Phone { get; set; }
+
+		[Required(ErrorMessage = "Czy sie zjawisz ziom?")]
+		public bool? WillAttend { get; set; }
+	}
+
+}
